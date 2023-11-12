bd17fad0f65a5c234f7fe3c925952cba5c21e5c0
Nathan Winslow <110609695+Nate-NAI@users.noreply.github.com>
2023-11-12 13:17:37 -0500

    Update char_sheet_test.py

--- v1.1/char_sheet_test.py
+++ v1.1/char_sheet_test.py
@@ -4,15 +4,20 @@ Module for testing future character sheet classes.
 __author__ = "Nathan Winslow"
 __copyright__ = "MIT"
 
-from character_sheet import CharacterSheet
+from character_sheet import PulpCharacter
         
 # Test values        
 example_character = 'pulp_cthulhu_sheet.json'
-my_character = CharacterSheet(example_character)
-test_key = 'CON'
+my_character = PulpCharacter(example_character)
+test_key = 'Latin'
+
+
+def print_skills_test(skill_list):
+    print('\n----------- Skill List -----------')
+    for skill in skill_list:
+        print(skill)
+
 
 print(f"{my_character.name}({my_character.pronoun}), {my_character.age}")
-print(f"{test_key} score: {my_character.get_value_at(test_key)}")
-print('\n----------- Skill List -----------')
-for skill in my_character.skills:
-    print(skill)
+print(f"{test_key}: {my_character.get_value_at(test_key)}")
+

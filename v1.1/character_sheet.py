e7a9e981ce2cec0aa06ca570441d8ff511f0ef8d
Nathan Winslow <110609695+Nate-NAI@users.noreply.github.com>
2023-12-16 14:19:11 -0500

    Moved logic to game.py

--- v1.1/character_sheet.py
+++ v1.1/character_sheet.py
@@ -7,21 +7,32 @@ __copyright__ = "MIT"
 from json_parser import JSONParser
 from dice import CthulhuDice
 
-    
-class CthulhuCharacter:
-
+class PlayerCharacter:
     def __init__(self, fpath: str):
-        self.character_sheet = JSONParser.load_json_file(fpath)   
-        self.prev_skill_modifier = 0  # used when pushing rolls.
-        self.db = self.damage_bonus()
-        self.skills_to_improve = []  # Used during Development phase
-        
+        self.character_sheet = JSONParser.load_json_file(fpath)
+
     def __call__(self):
         return self.character_sheet
     
     def get_value_at(self, key: str):
-        value = JSONParser.get_value_at_key(self.character_sheet, key)
-        return value
+        return JSONParser.get_value_at_key(self.character_sheet, key)
+        
+    @property
+    def age(self):
+        return self.character_sheet['Age']
+    
+    @property
+    def name(self):
+        return self.character_sheet['Name']
+    
+
+class CthulhuCharacter(PlayerCharacter):
+
+    def __init__(self, fpath: str):
+        super().__init__(fpath)  
+        self.prev_skill_modifier: int = 0  # used when pushing rolls.
+        self.db: tuple = self.damage_bonus()
+        self.skills_to_improve: list[str] = []  # Used during Development phase
     
     def damage_bonus(self) -> tuple:
         """ Returns a tuple (num_dice, num_side) such that -2 and -1 are const"""
@@ -47,22 +58,12 @@ class CthulhuCharacter:
         print(f"Casting {spell_name}!")
         # TODO: Flesh out spell logic
 
-    def make_skill_roll(self, base_val: int, difficulty: str, bonus_die: int, penalty_die: int) -> bool:
-        """
-        makes a skill roll based on the difficulty 
-        with any modifiers (bonus/penatly dice)
-        
-        :note: this method does not account for fumbles.
-
-        :return: True if passed, False otherwise
-        """
-        skill_val = self.get_skill_at_difficulty(base_val, difficulty)
-        roll = CthulhuDice.roll_skill(bonus_die, penalty_die)
-        return True if roll <= skill_val else False
+    def roll_skill(self, bonus_die: int, penalty_die: int):
+        return CthulhuDice.roll_skill(bonus_die, penalty_die)
 
     def get_skill_at_difficulty(self, skill_val: int, level: str) -> int:
         if level == "Hard":
-            return skill_val // 2      
+            return skill_val // 2
         elif level == "Extreme":
             return skill_val // 5
         else:
@@ -87,14 +88,6 @@ class CthulhuCharacter:
     def change_sanity(self, amount: int):
         """ same behavior as change_hit_points """
         self.character_sheet['Characteristics']['Sanity']['Current'] += amount
-    
-    @property
-    def age(self):
-        return self.character_sheet['Age']
-    
-    @property
-    def name(self):
-        return self.character_sheet['Name']
     
     @property
     def pronoun(self):
